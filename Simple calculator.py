c1dc60080964a2cc4249898ee639ae3ba94ea25d
Mila Podchasova <Pod4asova20@gmail.com>
2025-04-06 08:25:03 +0300

    Create Simple calculator.py

---
+++ Simple calculator.py
@@ -0,0 +1,20 @@
+# Simple calculator
+try:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter an operation (+, -, *, /): ")
+
+    if operation == "+":
+        print("Result:", num1 + num2)
+    elif operation == "-":
+        print("Result:", num1 - num2)
+    elif operation == "*":
+        print("Result:", num1 * num2)
+    elif operation == "/":
+        print("Result:", num1 / num2)
+    else:
+        print("Invalid operation")
+except ValueError:
+    print("Please enter valid numbers.")
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
