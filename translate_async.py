35f5dc851af6204a4ad2f6414134c6471c328ec9
Mila Podchasova <Pod4asova20@gmail.com>
2025-04-21 14:03:40 +0300

    Create translate_async.py

---
+++ translate_async.py
@@ -0,0 +1,37 @@
+import asyncio  
+from deep_translator import GoogleTranslator
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+
+# Пул потоков для выполнения синхронной функции в фоновом режиме
+executor = ThreadPoolExecutor()
+
+def sync_translate(text, target_language):
+    result = GoogleTranslator(target=target_language).translate(text)
+    return result
+
+async def async_translate(text, target_language):
+    loop = asyncio.get_event_loop()
+    # Используем executor для выполнения синхронного вызова
+    result = await loop.run_in_executor(executor, sync_translate, text, target_language)
+    return result
+
+async def main():
+    start = datetime.now()
+    print(f"start {start}")
+
+    with open('us_eng.txt', 'r', encoding='utf-8') as file:
+        texts = file.readlines()
+        tasks = [async_translate(text, 'ru') for text in texts]
+        results = await asyncio.gather(*tasks)
+        for result in results:
+            print(result)
+
+    end = datetime.now()
+    print(f"start {end}")
+    print(f"total time: {end-start}")
+
+
+# Запуск асинхронной функции
+if __name__ == "__main__":
+    asyncio.run(main())
